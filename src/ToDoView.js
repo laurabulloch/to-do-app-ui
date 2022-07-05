32695677a093fd05f2d415083825b8c0ee3ce19b
laurabulloch <s1928598@ed.ac.uk>
2022-07-05 12:13:09 +0100

    configure eslint and prettier

--- src/ToDoView.js
+++ src/ToDoView.js
@@ -1,63 +1,56 @@
-
-import {Button, Dialog, DialogContent, DialogTitle, List, ListItem, ListItemText, TextField} from "@mui/material";
-import {useEffect, useState} from "react";
-import axios from "axios";
+import { Button, Dialog, DialogContent, DialogTitle, List, ListItem, ListItemText, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function ToDoView() {
-    const [open, setOpen] = useState(false);
-    const [name, setName] = useState('');
-    const [toDos, setToDos] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [toDos, setToDos] = useState([]);
 
-    useEffect(() => {
-        axios.get(`/to-dos`).then(response => {
-            setToDos(response.data);
-        })
-    }, []);
+  useEffect(() => {
+    axios.get(`/to-dos`).then((response) => {
+      setToDos(response.data);
+    });
+  }, []);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleAddItem = () => {
-        axios
-            .post(`/to-dos`, {
-                name,
-            })
-            .then((response) => {
-                const updatedToDos = [...toDos, response.data]
-                setToDos(updatedToDos);
-            });
-        handleClose()
-    };
+  const handleAddItem = () => {
+    axios
+      .post(`/to-dos`, {
+        name,
+      })
+      .then((response) => {
+        const updatedToDos = [...toDos, response.data];
+        setToDos(updatedToDos);
+      });
+    handleClose();
+  };
 
-    return (
-        <div>
-            <List>
-                {toDos.map((item) => (
-                    <ListItem key={item.id}>
-                        <ListItemText primary={item.name} />
-                    </ListItem>
-                ))}
-            </List>
-            <Button onClick={handleClickOpen}>
-                + Add To Do
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Add New To Do</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        label="Enter To Do here"
-                        value = {name}
-                        onChange = {(event) => setName(event.target.value)}
-                    />
-                </DialogContent>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleAddItem}>Add To Do</Button>
-            </Dialog>
-        </div>
-    )
+  return (
+    <div>
+      <List>
+        {toDos.map((item) => (
+          <ListItem key={item.id}>
+            <ListItemText primary={item.name} />
+          </ListItem>
+        ))}
+      </List>
+      <Button onClick={handleClickOpen}>+ Add To Do</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add New To Do</DialogTitle>
+        <DialogContent>
+          <TextField label="Enter To Do here" value={name} onChange={(event) => setName(event.target.value)} />
+        </DialogContent>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleAddItem}>Add To Do</Button>
+      </Dialog>
+    </div>
+  );
 }
--- src/ToDoView.test.js
+++ src/ToDoView.test.js
@@ -1,8 +1,8 @@
 import ToDoView from './ToDoView';
-import userEvent from "@testing-library/user-event";
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react';
-import axios from "axios";
-import {act} from "react-dom/test-utils";
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import axios from 'axios';
+import { act } from 'react-dom/test-utils';
 
 const addButton = () => screen.getByRole('button', '+ Add To Do');
 const saveButton = () => screen.getByText('Add To Do');
@@ -12,76 +12,76 @@ const toDoTextField = () => screen.getByLabelText('Enter To Do here');
 jest.mock('axios');
 
 describe('To Do View', () => {
-    beforeEach( async () => {
-        const toDos = [
-            {id:1, name: 'Item 1'},
-            {id:2, name: 'Item 2'},
-        ];
-        axios.get.mockResolvedValue({data: toDos})
-        await act(async () => {
-            render(<ToDoView/>)
-        });
+  beforeEach(async () => {
+    const toDos = [
+      { id: 1, name: 'Item 1' },
+      { id: 2, name: 'Item 2' },
+    ];
+    axios.get.mockResolvedValue({ data: toDos });
+    await act(async () => {
+      render(<ToDoView />);
     });
-    it('should have a button', () => {
-        expect(addButton()).toBeInTheDocument()
-    })
-    it('should contain a list', () => {
-        expect(screen.getByRole('list')).toBeInTheDocument();
-    })
-    it('should load ToDos', () =>{
-        expect(axios.get).toHaveBeenCalledWith('/to-dos');
-    })
-    it('should display ToDos',  () =>{
-        expect(screen.getByText('Item 1')).toBeInTheDocument();
-        expect(screen.getByText('Item 2')).toBeInTheDocument();
-    })
-    describe("add", () => {
-        beforeEach( () => {
-            userEvent.click(addButton());
-        })
-        it('should display a pop up', () => {
-            expect(screen.getByRole('dialog')).toBeInTheDocument()
-        })
-        it('should have save button on dialog box', () => {
-            expect(saveButton()).toBeInTheDocument();
-        })
-        it('should have cancel button on dialog box', () => {
-            expect(cancelButton()).toBeInTheDocument();
-        })
-        it('should have input box on dialog', () => {
-            expect(toDoTextField()).toBeInTheDocument();
-        })
-        it('should close dialog on cancel', (done) => {
-            userEvent.click(cancelButton());
+  });
+  it('should have a button', () => {
+    expect(addButton()).toBeInTheDocument();
+  });
+  it('should contain a list', () => {
+    expect(screen.getByRole('list')).toBeInTheDocument();
+  });
+  it('should load ToDos', () => {
+    expect(axios.get).toHaveBeenCalledWith('/to-dos');
+  });
+  it('should display ToDos', () => {
+    expect(screen.getByText('Item 1')).toBeInTheDocument();
+    expect(screen.getByText('Item 2')).toBeInTheDocument();
+  });
+  describe('add', () => {
+    beforeEach(() => {
+      userEvent.click(addButton());
+    });
+    it('should display a pop up', () => {
+      expect(screen.getByRole('dialog')).toBeInTheDocument();
+    });
+    it('should have save button on dialog box', () => {
+      expect(saveButton()).toBeInTheDocument();
+    });
+    it('should have cancel button on dialog box', () => {
+      expect(cancelButton()).toBeInTheDocument();
+    });
+    it('should have input box on dialog', () => {
+      expect(toDoTextField()).toBeInTheDocument();
+    });
+    it('should close dialog on cancel', (done) => {
+      userEvent.click(cancelButton());
 
-            waitForElementToBeRemoved(screen.queryByRole('dialog')).then(() => {
-                expect(screen.queryByLabelText('Add New To Do')).not.toBeInTheDocument();
-                done();
-            });
-        })
-        it('should post on save',  () => {
-            axios.post.mockImplementation(() => new Promise(jest.fn()));
+      waitForElementToBeRemoved(screen.queryByRole('dialog')).then(() => {
+        expect(screen.queryByLabelText('Add New To Do')).not.toBeInTheDocument();
+        done();
+      });
+    });
+    it('should post on save', () => {
+      axios.post.mockImplementation(() => new Promise(jest.fn()));
 
-            userEvent.type(toDoTextField(), 'Item');
-            userEvent.click(saveButton());
+      userEvent.type(toDoTextField(), 'Item');
+      userEvent.click(saveButton());
 
-            expect(axios.post).toHaveBeenCalledWith('/to-dos', {name: 'Item'});
-        })
-        it('should add to list on user input', async() => {
-            axios.post.mockResolvedValue({
-                data: {
-                    id: 3,
-                    name: 'Item 3'
-                }
-            });
+      expect(axios.post).toHaveBeenCalledWith('/to-dos', { name: 'Item' });
+    });
+    it('should add to list on user input', async () => {
+      axios.post.mockResolvedValue({
+        data: {
+          id: 3,
+          name: 'Item 3',
+        },
+      });
 
-            userEvent.type(toDoTextField(), 'Item 3');
+      userEvent.type(toDoTextField(), 'Item 3');
 
-            await act(async () => {
-                userEvent.click(saveButton());
-            });
+      await act(async () => {
+        userEvent.click(saveButton());
+      });
 
-            expect(screen.getByText('Item 3')).toBeInTheDocument();
-        })
-    })
+      expect(screen.getByText('Item 3')).toBeInTheDocument();
+    });
+  });
 });
